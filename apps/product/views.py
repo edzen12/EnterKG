ac5ab571e3b3430a3da15a48b5c6a08732d15f0c
edzen12 <oichiev.edzen@gmail.com>
2026-06-09 19:06:23 +0600

    html and view

--- apps/product/views.py
+++ apps/product/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 from apps.category.models import Category
 from apps.product.models import Product
+
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -19,3 +20,18 @@ class HomeView(TemplateView):
             )[:12]
         )
         return context
+    
+
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = 'category_detail.html'
+    context_object_name = 'category'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = (
+            Category.objects.filter(
+                parent__isnull=True, is_active=True
+            ).prefetch_related('children')
+        )
+        context['']
