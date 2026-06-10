f88ff7e1bd61bb9d88f14164516a2042be9ba755
edzen12 <oichiev.edzen@gmail.com>
2026-06-10 19:28:28 +0600

    done front product

--- apps/product/models.py
+++ apps/product/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products/')
     description = models.TextField(blank=True,null=True)
     price_usd = models.DecimalField(
-        max_digits=10, decimal_places=2)
+        max_digits=10, decimal_places=2, verbose_name='Цена сом')
     stock = models.PositiveIntegerField(default=0)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
